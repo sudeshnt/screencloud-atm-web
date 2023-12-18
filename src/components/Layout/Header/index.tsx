5ff495ff3ed07e71254aa9a283140836746fd52c
Sudesh Nimesha <sudeshnt93@gmail.com>
2023-12-18 16:20:14 +0530

    adjust header

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,12 +1,29 @@
+import { HStack } from '@chakra-ui/react';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className='px-[8%]'>
+    <header className='flex justify-center px-[8%] py-8'>
       <nav>
         <Link href='/'>
-          <Image src='/images/logo.png' width={320} height={77} alt='' priority />
+          <HStack alignItems='center'>
+            <Image
+              src='/images/screencloud.png'
+              width={150}
+              height={50}
+              alt=''
+              priority
+            />
+            <Image
+              src='/images/logo.svg'
+              width={500}
+              height={50}
+              alt=''
+              className='-ml-6 -mt-14'
+              priority
+            />
+          </HStack>
         </Link>
       </nav>
     </header>
